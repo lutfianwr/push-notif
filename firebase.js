35d0aef6b3176fdf79f7c19fe1041fbbe357b3c4
lutfi nuril anwar <lutfi.nurilanwar@gmail.com>
2023-01-17 14:18:12 +0700

    init

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+initializeApp(firebaseConfig);
